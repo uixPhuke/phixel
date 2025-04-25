8907d79090e469b028a88c41f3efac7a54cdebe9
uixPhuke <uixphuke@gmail.com>
2025-04-25 21:30:46 +0530

    database set

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const URI = process.env.MONGO_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("MongoDB connected successfully");
+    // Optional: Set up connection event listeners
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    db.once("open", () => {
+      console.log("MongoDB connection opened");
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express=require('express')
 const app=express()
 require('dotenv').config()
 
-
+const connectDB = require('./config/db')
 const PORT=process.env.PORT
 
+//connect to database
+connectDB()
+
+//start server
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 }
 )
+//api routes
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 }
