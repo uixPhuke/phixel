146ac948cf168aaeb76227538961e6a24576e882
uixPhuke <uixphuke@gmail.com>
2025-04-25 21:09:36 +0530

    server set

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+require('dotenv').config()
+
+
+const PORT=process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+}
+)
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+}
+)
