c875f04982b54de87d20dc5baa3edb9fcfcf777f
uixPhuke <uixphuke@gmail.com>
2025-05-19 23:26:44 +0530

    user update functionality

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,13 +1,30 @@
 const User = require("../models/userSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const {createToken} = require("../middlewares/auth");
+const { createToken } = require("../middlewares/auth");
 
 // Register a new user
 const registerUser = async (req, res) => {
-  const { firstName, lastName, username, email, phone, dob, password,isAdmin } = req.body;
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    phone,
+    dob,
+    password,
+    isAdmin,
+  } = req.body;
 
-  if (!firstName || !lastName || !username || !email || !phone || !dob || !password) {
+  if (
+    !firstName ||
+    !lastName ||
+    !username ||
+    !email ||
+    !phone ||
+    !dob ||
+    !password
+  ) {
     return res.status(400).json({
       success: false,
       message: "Please fill all the fields",
@@ -26,10 +43,13 @@ const registerUser = async (req, res) => {
       message: "Please enter a valid phone number",
     });
   }
-  if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
+  if (
+    !/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)
+  ) {
     return res.status(400).json({
       success: false,
-      message: "Password must contain at least 1 uppercase letter, 1 number, 1 special character, and be at least 8 characters long",
+      message:
+        "Password must contain at least 1 uppercase letter, 1 number, 1 special character, and be at least 8 characters long",
     });
   }
 
@@ -58,10 +78,10 @@ const registerUser = async (req, res) => {
         message: "Phone number already exists",
       });
     }
-  //Hashed the password
+    //Hashed the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-//create a new user
+    //create a new user
     const newUser = new User({
       firstName,
       lastName,
@@ -70,18 +90,17 @@ const registerUser = async (req, res) => {
       phone,
       dob,
       password: hashedPassword,
-      isAdmin: isAdmin || false
+      isAdmin: isAdmin || false,
     });
 
     await newUser.save();
-   const token= createToken(newUser._id, newUser.email, res);
-     res.status(201).json({
+    const token = createToken(newUser._id, newUser.email, res);
+    res.status(201).json({
       success: true,
       message: "User registered successfully",
       user: newUser,
-      token
+      token,
     });
-
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -92,54 +111,129 @@ const registerUser = async (req, res) => {
 };
 
 //login user
-const loginUser=async(req,res)=>{
-    const {emailOrUsername,password}=req.body;
-    if(!emailOrUsername || !password){
-        return res.status(400).json({
-            success:false,
-            message:"Please fill all the fields"
-        })
-    }
-    // Check if password is at least 6 characters long
-    if (password.length < 6) {
+const loginUser = async (req, res) => {
+  const { emailOrUsername, password } = req.body;
+  if (!emailOrUsername || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Please fill all the fields",
+    });
+  }
+  // Check if password is at least 6 characters long
+  if (password.length < 6) {
+    return res.status(400).json({
+      success: false,
+      message: "Password must be at least 6 characters long",
+    });
+  }
+  try {
+    const user = await User.findOne({
+      $or: [{ email: emailOrUsername }, { username: emailOrUsername }],
+    });
+    if (!user) {
       return res.status(400).json({
         success: false,
-        message: 'Password must be at least 6 characters long',
+        message: "Invalid credentials",
       });
     }
-    try{
-        const user=await User.findOne({
-          $or: [
-            { email: emailOrUsername },
-            { username: emailOrUsername },
-          ]});
-        if(!user){
-            return res.status(400).json({
-                success:false,
-                message:"Invalid credentials"
-            })
-        }
-        const isMatch=await bcrypt.compare(password,user.password);
-        if(!isMatch){
-            return res.status(400).json({
-                success:false,
-                message:"Invalid credentials"
-            })
-        }
-        const token=createToken(user._id,user.email,res);
-        res.status(200).json({
-            success:true,
-            message:"User logged in successfully",
-            user,
-            token
-        })
-    }catch(error){
-        return res.status(500).json({
-            success:false,
-            message:"Internal server error",
-            error:error.message
-        })
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
     }
-}
+    const token = createToken(user._id, user.email, res);
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
-module.exports = { registerUser,loginUser };
+//update User
+const updateUser = async (req, res, next) => {
+  try {
+    const { firstName, lastName, username, email, phone, dob } = req.body;
+    if (!firstName || !lastName || !username || !email || !phone || !dob) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the fields",
+      });
+    }
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Check if the username is already taken by another user
+    const usernameExists = await User.findOne({
+      username,
+      _id: { $ne: user._id },
+    });
+
+    if (usernameExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Username is already taken",
+      });
+    }
+
+    // Check if the email is already taken by another user
+    const emailExists = await User.findOne({
+      email,
+      _id: { $ne: user._id },
+    });
+
+    if (emailExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is already taken",
+      });
+    }
+    // Check if the phone number is already taken by another user
+    const phoneExists = await User.findOne({
+      phone,
+      _id: { $ne: user._id },
+    });
+    if (phoneExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Phone number is already taken",
+      });
+    }
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.username = username;
+    user.email = email;
+    user.phone = phone;
+    user.dob = dob;
+
+    const updateUser = await user.save();
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      updateUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, updateUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser,loginUser } = require('../controllers/userCtrl');
+const { registerUser,loginUser,updateUser } = require('../controllers/userCtrl');
 const {isAuthenticated}= require('../middlewares/auth');
 
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.route('/update').put(isAuthenticated,updateUser);
 
 module.exports = router;
 
