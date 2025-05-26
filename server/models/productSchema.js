9445eaffefe9703ba73b3750670d72fd6607ddd3
uixPhuke <uixphuke@gmail.com>
2025-05-26 22:12:19 +0530

    mistake undo

--- server/models/productSchema.js
+++ server/models/productSchema.js
@@ -145,4 +145,4 @@ const productSchema= new mongoose.Schema({
 // productSchema.index({ collarType: 1 });
 
 const Product = mongoose.model('Product', productSchema);
-module.exports = Product;images
+module.exports = Product;
