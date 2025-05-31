37fcf025613a5a95d09bf18fdcafa0a0bdcaa46e
uixPhuke <uixphuke@gmail.com>
2025-05-31 12:51:17 +0530

    cart schema

---
+++ server/models/cartSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cartSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+},{timestamps: true});
+
+const Cart = mongoose.model('Cart', cartSchema);
+module.exports = Cart;
