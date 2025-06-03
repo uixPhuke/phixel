c8683f27c201ed5cf0258d390b050d8921fb9a66
uixPhuke <uixphuke@gmail.com>
2025-06-03 19:32:52 +0530

    cart routes

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const router=express.Router();
+const { getDbCart,
+    addToDbCart,
+    syncGuestCart,
+    removeFromCart,
+    clearCart } = require('../controllers/cartCtrl');
+const { isAuthenticated } = require('../middlewares/auth');
+// Route to get user's cart from DB
+router.get('/cart', isAuthenticated, getDbCart);
+// Route to add item to user's cart in DB
+router.post('/cart', isAuthenticated, addToDbCart);
+// Route to sync guest cart with user's DB cart
+router.post('/cart/sync', isAuthenticated, syncGuestCart);
+// Route to remove item from user's cart in DB
+router.delete('/cart/:productId', isAuthenticated, removeFromCart);
+// Route to clear user's cart in DB
+router.delete('/cart', isAuthenticated, clearCart);     
+
+// Export the router
+module.exports = router;
