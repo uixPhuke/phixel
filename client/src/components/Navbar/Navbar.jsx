9575d50f871abb0ec8768186b2a9e5b88232caf0
uixPhuke <uixphuke@gmail.com>
2025-06-25 21:23:00 +0530

    navbar navlink

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,17 +2,45 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import SearchBar from '../../pages/SearchBar/SearchBar'
+import { FaRegHeart, FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <header className={`fixed w-full bg-secondary  shadow-md z-50 p-4`}>
+    <header className={`fixed w-full bg-secondary shadow-md  z-50 p-6 pl-12 pr-12`}>
       <div className="flex mx-auto items-center justify-between">
+        {/* Logo */}
         <div className="flex items-center">
           <Link to='/'>
-          <img src={logo} className='h-8' alt="uixLogo"/></Link>
+          <img src={logo} className='h-16' alt="uixLogo"/></Link>
         </div>
+        {/* Navigation Links for Large Screens */}
+        <nav className="text-primary hidden md:flex md:items-center space-x-20 text-xs  transition-all duration-300">
+          <Link to="/men" className="hover:text-accent">
+            MEN
+          </Link>
+          <Link to="/women" className="hover:text-accent">
+            WOMEN
+          </Link>
+          <Link to="/kids" className="hover:text-accent">
+            KIDS
+          </Link>
+          <Link to="/unisex" className="hover:text-accent">
+            UNISEX
+          </Link>
+
+          
+            
+         
+        </nav>
+        
         {/* Search Bar */}
         <SearchBar />
+        
+        {/* Cart */}
+        <Link to="/wishlist" className="hover:text-accent text-sm">
+              <FaRegHeart className=" transition-all duration-300" />
+        </Link>
+
       </div>
 
 
--- client/src/pages/SearchBar/SearchBar.jsx
+++ client/src/pages/SearchBar/SearchBar.jsx
@@ -4,14 +4,15 @@ import { HiOutlineSearch } from "react-icons/hi";
 const SearchBar = () => {
   return (
      
-     <div className="flex items-center">
+     <div className="flex items-center  border-primary border-0 rounded-full shadow-md ">
      <div className="relative">
        <input
          type="text"
          placeholder="search"
-         className="bg-accent text-primary pl-4 pr-8 py-2 rounded-full text-sm focus:outline-none transition-all duration-300"
+         className="bg-secondary text-primary pl-4 pr-8 py-2 rounded-full text-xs focus:outline-none transition-all duration-300 hover:bg-accent focus:bg-accent focus:border-primary w-full"
        />
-       <HiOutlineSearch className="absolute top-1/2 right-2 transform -translate-y-1/2 text-primary" />
+       <HiOutlineSearch className="absolute top-1/2 right-2 transform -translate-y-1/2 text-primary text-sm " />
+       
      </div>
    </div>
   )
