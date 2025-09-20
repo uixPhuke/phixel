c381afd4d7b129b6218322159931a2f93ef5cd7c
uixPhuke <uixphuke@gmail.com>
2025-09-20 23:23:24 +0530

    updating home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,6 +4,13 @@ const Home = () => {
   return (
     <div>
       <h3>Welcome to the Home Page</h3>
+      <div>
+        <h2 className='scroll-px-px'>Welcome to the Home Page</h2>
+        <p className='scroll-px-px'>This is a simple React application demonstrating routing and basic component structure.</p>
+        <p className='scroll-px-px'>Use the navigation links to explore different pages.</p>
+        <p className='scroll-px-px'>Feel free to modify and expand the application as needed!</p> 
+        
+      </div>
     </div>
   )
 }
