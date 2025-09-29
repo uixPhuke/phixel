6cce6dc75b9d0a095b9ed43402880e7d62ccc1e0
uixPhuke <uixphuke@gmail.com>
2025-09-30 00:26:28 +0530

    adding product constants

---
+++ client/src/constants/productConstants.js
@@ -0,0 +1,30 @@
+export const PRODUCT_CATEGORIES = [
+  'tshirts', 'shirts', 'jeans', 'jackets', 'hoodies', 
+  'dresses', 'skirts', 'shorts', 'pants', 'ethnic', 
+  'formal', 'casual', 'activewear'
+];
+
+export const PRODUCT_SIZES = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+
+export const FIT_TYPES = ["Slim Fit", "Regular Fit", "Loose Fit", "Oversized"];
+
+export const PATTERNS = [
+  "Solid", "Striped", "Checked", "Floral", "Printed", "Graphic", "Abstract"
+];
+
+export const SLEEVE_TYPES = [
+  "Full Sleeve", "Half Sleeve", "Sleeveless", "Cap Sleeve", "Three-Quarter Sleeve"
+];
+
+export const COLLAR_TYPES = [
+  "Round Neck", "V Neck", "Polo", "Turtleneck", "Collared", "Mandarin Collar"
+];
+
+export const GENDER_TYPES = ['Men', 'Women', 'Unisex', 'Kids'];
+
+export const SORT_OPTIONS = [
+  { value: 'newest', label: 'Newest' },
+  { value: 'oldest', label: 'Oldest' },
+  { value: 'price', label: 'Price: Low to High' },
+  { value: 'name', label: 'Name: A-Z' }
+];
