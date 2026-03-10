8c30faa36893bd2942db41da86a0771ce26d5cb3
uixPhuke <uixphuke@gmail.com>
2026-03-10 20:44:24 +0530

    remove getCArt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   useEffect(() => {
     dispatch(verify());
     dispatch(getWishlist());
-      dispatch(getCart());
+      // dispatch(getCart());
     
   }, [dispatch]);
 
