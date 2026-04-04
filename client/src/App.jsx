88c1142810fa4a2e4da409ee34568fd8a959a87a
uixPhuke <uixphuke@gmail.com>
2026-04-04 19:15:20 +0530

    feat: refactor App component structure and improve routing with ScrollToTop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,109 +1,113 @@
+
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./pages/Home/Hero";
 import About from "./pages/Home/About";
 import { Register } from "./pages/Auth/Register";
-import { getWishlist } from "./actions/wishlistActions";
 import { Login } from "./pages/Auth/Login";
+import LoginModal from "./pages/Auth/LoginModal";
 import CartPage from "./pages/Cart/Cart";
-import { ProductListingPage, ProductDetailPage } from "./pages/Product";
+import {
+  ProductListingPage,
+  ProductDetailPage,
+} from "./pages/Product";
 import Wishlist from "./pages/Wishlist/Wishlist";
 import { Toaster } from "react-hot-toast";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
 import { verify } from "./actions/userActions";
-import { getCart } from "./actions/cartActions";  
+import { getWishlist } from "./actions/wishlistActions";
+import { getCart } from "./actions/cartActions";
+
 import Profile from "./pages/Profile/Profile";
 import Checkout from "./pages/Checkout/Checkout";
 import Payment from "./pages/Checkout/Payment";
+
 import Footer from "./components/Footer/Footer";
 import CategorySpotlight from "./pages/Home/CategorySpotlight";
-import { setShowLoginModalFalse } from "./slices/userSlice";
-import { useSelector } from "react-redux";
-import LoginModal from "./pages/Auth/LoginModal";
+import ScrollToTop from "./components/ScrollToTop";
 
 import AdminRoute from "./components/Global/AdminRoute";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
-
 import AdminProductsPage from "./pages/Product/AdminProductsPage";
 import AdminProductFormPage from "./pages/Product/AdminProductFormPage";
 
-const App = () => {
+import { setShowLoginModalFalse } from "./slices/userSlice";
 
+const App = () => {
   const dispatch = useDispatch();
-   const { showLoginModal } = useSelector((state) => state.user);
+  const { showLoginModal } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(verify());
     dispatch(getWishlist());
-       dispatch(getCart());
-    
+    dispatch(getCart());
   }, [dispatch]);
 
   return (
-    <BrowserRouter>
-    
+    <>
       <Navbar />
-<LoginModal
+
+      <LoginModal
         isOpen={showLoginModal}
         onClose={() => dispatch(setShowLoginModalFalse())}
       />
 
-<Toaster
-  position="top-center"
-  toastOptions={{
-    duration: 2500,
-    style: {
-      background: "#fff",
-      color: "#000",
-      padding: "14px 20px",
-      fontSize: "14px",
-      borderRadius: "30px",
-      fontWeight: "500",
-      letterSpacing: "0.3px",
-    },
-  }}
-/>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 2500,
+          style: {
+            background: "#fff",
+            color: "#000",
+            padding: "14px 20px",
+            fontSize: "14px",
+            borderRadius: "30px",
+            fontWeight: "500",
+            letterSpacing: "0.3px",
+          },
+        }}
+      />
+
       <div className="pt-[60px] space-y-24">
         <Routes>
+          {/* Admin Routes */}
+          <Route
+            path="/admin/dashboard"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          />
 
-          {/* Admin Dashboard */}
-          {/* Admin Dashboard */}
-<Route
-  path="/admin/dashboard"
-  element={
-    <AdminRoute>
-      <AdminDashboard />
-    </AdminRoute>
-  }
-/>
+          <Route
+            path="/admin/products"
+            element={
+              <AdminRoute>
+                <AdminProductsPage />
+              </AdminRoute>
+            }
+          />
 
-<Route
-  path="/admin/products"
-  element={
-    <AdminRoute>
-      <AdminProductsPage />
-    </AdminRoute>
-  }
-/>
+          <Route
+            path="/admin/products/create"
+            element={
+              <AdminRoute>
+                <AdminProductFormPage />
+              </AdminRoute>
+            }
+          />
 
-<Route
-  path="/admin/products/create"
-  element={
-    <AdminRoute>
-      <AdminProductFormPage />
-    </AdminRoute>
-  }
-/>
-
-<Route
-  path="/admin/products/edit/:productID"
-  element={
-    <AdminRoute>
-      <AdminProductFormPage />
-    </AdminRoute>
-  }
-/>
+          <Route
+            path="/admin/products/edit/:productID"
+            element={
+              <AdminRoute>
+                <AdminProductFormPage />
+              </AdminRoute>
+            }
+          />
 
           {/* Home */}
           <Route
@@ -111,12 +115,13 @@ const App = () => {
             element={
               <>
                 <Hero />
-                <CategorySpotlight/>
-              
+                <CategorySpotlight />
               </>
             }
           />
-          <Route path='/profile' element={<Profile />} />
+
+          {/* User */}
+          <Route path="/profile" element={<Profile />} />
 
           {/* Auth */}
           <Route path="/register" element={<Register />} />
@@ -124,7 +129,10 @@ const App = () => {
 
           {/* Products */}
           <Route path="/products" element={<ProductListingPage />} />
-          <Route path="/products/:productID" element={<ProductDetailPage />} />
+          <Route
+            path="/products/:productID"
+            element={<ProductDetailPage />}
+          />
 
           {/* Cart */}
           <Route path="/cart" element={<CartPage />} />
@@ -138,14 +146,21 @@ const App = () => {
 
           {/* 404 */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
-
         </Routes>
-         
       </div>
-       <About />
-        <Footer/>
-    </BrowserRouter>
+
+      <About />
+      <Footer />
+    </>
   );
 };
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
+  );
+}
+
